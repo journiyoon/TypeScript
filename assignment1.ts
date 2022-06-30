558361e3a4c87e796292938fcedae2503867282e
journiyoon <journiyoon@gmail.com>
2022-07-01 01:12:57 +0900

    feat: function prepend

--- assignment1.ts
+++ assignment1.ts
@@ -3,3 +3,11 @@ type Last = {
 };
 
 const last: Last = (arr) => arr[arr.length - 1];
+
+type Prepend = {
+	<T, V>(arr: T[], item: V): void;
+};
+
+const prepend: Prepend = (arr, item) => {
+	return [item, ...arr];
+};
